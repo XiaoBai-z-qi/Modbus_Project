2677904d61444e4bccca28c6eee5264e4f681fac
huojimian <2630319814@qq.com>
2026-03-16 16:38:22 +0800

    创建BootLoard工程

---
+++ STM32F4xx_BL/BL文档/BL.md
@@ -0,0 +1,9 @@
+# 一、STM32F411CEU6 片上Flash分区
+
+![flash分区](D:\FreeRTOS\Modbus_Project\STM32F4xx_BL\BL文档\BL.assets\flash分区.png)
+
+- <span style="color:#FF6666; font-size:1.2em;">**STM32F411CEU6 Flash 大小：512 KB**</span>
+  - **BootLoard区：**48 KB 
+  - **Flag区：**16 KB 
+  - **App区：**448 KB 
+
---
+++ STM32F4xx_BL/MDK-ARM/RTE/_STM32F4xx_BL/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32F4xx_BL' 
+ * Target:  'STM32F4xx_BL' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
